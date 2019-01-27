72f26296c138f0388348cb4114d1d3ceaaaf69fa
jld3103 <jld3103yt@gmail.com>
2019-01-27 15:03:16 +0100

    Removed dev flag from script

--- minutely.sh
+++ minutely.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p out out/replacementplan out/replacementplan/today out/replacementplan/tomorrow
-./node_modules/.bin/ts-node src/replacementplan/replacementplan.ts --dev
+./node_modules/.bin/ts-node src/replacementplan/replacementplan.ts
