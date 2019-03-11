1ebb06e99b175a95d9a297ffe915e9f9ee39dbcd
Finn <tupfi2@gmx.de>
2019-03-11 17:57:08 +0100

    Fixed teachers url

--- src/teachers/teachers.ts
+++ src/teachers/teachers.ts
@@ -18,7 +18,7 @@ const fetchData = (file: string) => {
             cookieJar, body: form
         });
         const stream = fs.createWriteStream(file);
-        got.stream('https://viktoriaschule-aachen.de/dokumente/upload/d7716_Lehrer_F%C3%A4cher_20190213.pdf', {
+        got.stream('https://viktoriaschule-aachen.de/dokumente/upload/3058d_Lehrer_Fakultenliste_20190307.pdf', {
             auth: config.username + ':' + config.password,
             cookieJar
         }).pipe(stream);
