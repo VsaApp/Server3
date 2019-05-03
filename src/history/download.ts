82ab67cf7f0793704a17b5493cebf0cb72cc2168
Finn <tupfi2@gmx.de>
2019-05-03 11:11:15 +0200

    Added authentication to history download

--- src/history/download.ts
+++ src/history/download.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import got from 'got';
+import config from '../config';
 
 const isCli = module.parent === null;
+const auth = `${config.username}:${config.password}`;
 
 const saveNewVersion = (directoriy: string, data: any, year: string, month: string, day: string, fileName: string) => {
 
@@ -31,7 +33,7 @@ export const downloadHistory = async () => {
     const directories = ["replacementplan", "unitplan"];
     for (let h = 0; h < directories.length; h++){
         console.log(`download ${directories[h]}`);
-        const years = await getJson(`https://history.api.vsa.2bad2c0.de/${directories[h]}`);
+        const years = await getJson(`https://${auth}@history.api.vsa.2bad2c0.de/${directories[h]}`);
         for (let i = 0; i < years.length; i++){
             console.log(`   - download year ${years[i].year}`);
             const months = years[i].months;
@@ -42,7 +44,7 @@ export const downloadHistory = async () => {
                     console.log(`         -- download day ${days[k].day}`);
                     const files = days[k].files;
                     for (let l = 0; l < files.length; l++){
-                        const fileName = `https://history.vsa.2bad2c0.de/${directories[h]}/${years[i].year}/${months[j].month}/${days[k].day}/${files[l]}`;
+                        const fileName = `https://${auth}@history.vsa.2bad2c0.de/${directories[h]}/${years[i].year}/${months[j].month}/${days[k].day}/${files[l]}`;
                         saveNewVersion(directories[h], (await got(fileName)).body, years[i].year, months[j].month, days[k].day, files[l]);
                     }
                 }
