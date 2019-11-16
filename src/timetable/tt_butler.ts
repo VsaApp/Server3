445ffacc6e846e48f74be474846870920c31aa7f
Finn <tupfi2@gmx.de>
2019-11-16 17:12:03 +0100

    Added the language to timetable notifications

--- src/timetable/tt_butler.ts
+++ src/timetable/tt_butler.ts
@@ -3,7 +3,7 @@ import download from './tt_download';
 import { sendNotification } from '../utils/notification';
 import { updateApp } from '../utils/update_app';
 import {getUsers} from '../tags/users';
-import { Timetables, User } from '../utils/interfaces';
+import { Timetables, User, Device } from '../utils/interfaces';
 import getAuth, { isDeveloper } from '../utils/auth';
 import { getGrade } from '../authentication/ldap';
 
@@ -17,21 +17,35 @@ timetableRouter.get('/', (req, res) => {
     return res.json(timetables.grades.get(grade));
 });
 
+/**
+ * Updates the timetable
+ */
 export const updateTimetable = async (): Promise<void> => {
     timetablePromise = download(timetables !== undefined)
     timetables = await timetablePromise || timetables;
 };
 
+/**
+ * Returns the version of the current timetable
+ */
 export const getTimetableVersion = (): string => {
     return timetables.date;
 };
 
+/**
+ * Returns the current timetable
+ */
 export const getTimetable = async (): Promise<Timetables | undefined> => {
     if (timetables !== undefined) return timetables;
     if (timetablePromise !== undefined) return await timetablePromise;
     return undefined;
 }
 
+/**
+ * Returns all subjects ids of one course id
+ * @param grade for timetable
+ * @param courseID searched [courseID]
+ */
 export const getSubjectIDsFromCourseID = (grade: string, courseID: string): string[] => {
     const timetable = timetables.grades.get(grade);
     if (!timetable) throw `Timetable for ${grade} is undefined!`;
@@ -45,20 +59,37 @@ export const getSubjectIDsFromCourseID = (grade: string, courseID: string): stri
     return subjectIDs;
 }
 
+/**
+ * Sends the new timetable notifications to all users
+ * @param isDev send only to developers (for debugging)
+ */
 export const sendNotifications = async (isDev: Boolean): Promise<void> => {
     try {
         let users = getUsers().filter((user: User) => (!isDev || isDeveloper(user.username)) && user.grade !== undefined);
         console.log('Sending notifications to ' + users.length + ' users');
-        await sendNotification({
-            users: users,
-            group: 'timetableChanged',
-            text: 'Es gibt einen neuen Stundenplan!',
-            title: 'Stundenplan',
-            data: {
-                type: 'timetable'
-            }
-        });
 
+        // Sort the devices by language
+        const languages = new Map<String, Device[]>();
+        users.forEach((user) => user.devices.forEach((device) => {
+            if (!languages.get(device.language)) languages.set(device.language, []);
+            const language = languages.get(device.language)
+            if (language) language.push(device);
+        }));
+
+        // Send notifications to each language
+        for (var language of Array.from(languages.keys())) {
+            await sendNotification({
+                devices: languages.get(language),
+                group: 'timetableChanged',
+                text: language === 'de' ? 'Es gibt einen neuen Stundenplan!' : 'There is a new timetable!',
+                title: language === 'de' ? 'Stundenplan' : 'Timetable',
+                data: {
+                    type: 'timetable'
+                }
+            });
+        }
+
+        // Inform the app about a new timetable
         await updateApp('All', {
             'type': 'timetable',
             'action': 'update'
