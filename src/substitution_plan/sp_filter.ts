5a767b72a737ce08e6c95f213288a12a7e2a5d1f
Finn <tupfi2@gmx.de>
2019-11-19 18:23:15 +0100

    Fixed substitution plan filter

--- src/substitution_plan/sp_filter.ts
+++ src/substitution_plan/sp_filter.ts
@@ -9,21 +9,25 @@ const filterSubstitutionPlan = async (substitutionPlan: SubstitutionPlan): Promi
             if (ttGrade) {
                 const ttDay = ttGrade.data.days[new Date(substitutionPlan.date).getDay() - 1];
                 substitutionPlan.data[grade].forEach((substitution) => {
-                    if (substitution.type != 2) {
-                        const ttUnit = ttDay.units[substitution.unit];
-                        const subjects = ttUnit.subjects.filter((subject) => {
-                            return subject.subjectID === substitution.original.subjectID &&
-                                subject.teacherID === substitution.original.teacherID;
-                        });
-                        if (subjects.length === 1) {
-                            substitution.id = subjects[0].id;
-                            substitution.courseID = subjects[0].courseID;
-                        } else {
-                            console.error(`Cannot filter grade: ${grade} unit: ${substitution.unit}`);
+                    try {
+                        if (substitution.type != 2) {
+                            const ttUnit = ttDay.units[substitution.unit];
+                            const subjects = ttUnit.subjects.filter((subject) => {
+                                return subject.subjectID === substitution.original.subjectID &&
+                                    subject.teacherID === substitution.original.teacherID;
+                            });
+                            if (subjects.length === 1) {
+                                substitution.id = subjects[0].id;
+                                substitution.courseID = subjects[0].courseID;
+                            } else {
+                                console.error(`Cannot filter grade: ${grade} unit: ${substitution.unit}`);
+                            }
                         }
-                    }
-                    if (substitution.original.course) {
-                        substitution.courseID = `${grade}-${substitution.original.course}-${substitution.original.subjectID}`;
+                        if (substitution.original.course) {
+                            substitution.courseID = `${grade}-${substitution.original.course}-${substitution.original.subjectID}`;
+                        }
+                    } catch (e) {
+                        console.error(`Failed to filter grade: ${grade} unit: ${substitution.unit}`);
                     }
                 });
             }
