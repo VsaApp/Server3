44f0e7165f0ff9512f4168bee00668ccf1b5ac3a
Finn <tupfi2@gmx.de>
2019-11-19 20:43:42 +0100

    Personalized notifications

--- src/substitution_plan/sp_filter.ts
+++ src/substitution_plan/sp_filter.ts
@@ -1,4 +1,4 @@
-import { SubstitutionPlan, Substitution, Timetable, Subject } from "../utils/interfaces";
+import { SubstitutionPlan, Substitution, Timetable, Subject, User } from "../utils/interfaces";
 import { getTimetable } from "../timetable/tt_butler";
 
 const filterSubstitutionPlan = async (substitutionPlan: SubstitutionPlan): Promise<SubstitutionPlan> => {
@@ -57,13 +57,39 @@ const filterSubstitutionPlan = async (substitutionPlan: SubstitutionPlan): Promi
  * @param substitution The [substitution] to fill
  * @param subject The [subject] with the [subjectID] of the [substitution]
  */
-const autoFillSubstitution = (substitution: Substitution, subject: Subject) => {
+const autoFillSubstitution = (substitution: Substitution, subject: Subject): void => {
     if (substitution.original.subjectID.length === 0) {
         substitution.original.subjectID = subject.subjectID;
     }
     if (substitution.original.roomID.length === 0) {
         substitution.original.roomID = subject.roomID;
     }
+}
+
+/**
+ * Returns all substitutions for a given user
+ * @param user The user to filter for
+ * @param day to search for
+ */
+export const getSubstitutionsForUser = (user: User, substitutionPlan: SubstitutionPlan): Substitution[] => {
+
+    // Reduces the ids to string arrays
+    const selectedCourses = user.selected.map((course) => course.courseID);
+    const selectedSubjectsIDs = user.selected.map((course) => course.subjectIDs).reduce((i1, i2) => Array.from(i1).concat(i2));
+
+    return substitutionPlan.data[user.grade].filter((substitution) => {
+        // If the server was not able to filter the substitution, select it and it will be marked as unknown
+        if (substitution.courseID === undefined && substitution.id === undefined) {
+            return true;
+        }
+        // Check if the substitution is selected
+        const selected = selectedCourses.includes(substitution.courseID || '-') || selectedSubjectsIDs.includes(substitution.id || '-');
+        // If it is an exam, check if writing is enabled
+        if (selected && substitution.type === 2) {
+            return user.exams.includes(substitution.courseID || '-');
+        }
+        return selected;
+    });
 }
 
 export default filterSubstitutionPlan;
--- src/substitution_plan/sp_notifications.ts
+++ src/substitution_plan/sp_notifications.ts
@@ -1,8 +1,10 @@
-import {getUsers} from '../tags/users';
-import {updateApp} from '../utils/update_app';
-import {sendNotification} from '../utils/notification';
+import { getUsers } from '../tags/users';
+import { updateApp } from '../utils/update_app';
+import { sendNotification } from '../utils/notification';
 import { User, Device, SubstitutionPlan } from '../utils/interfaces';
 import { isDeveloper } from '../utils/auth';
+import { getSubstitutionsForUser } from './sp_filter';
+import { getSubject } from '../utils/subjects';
 
 
 export const sendNotifications = async (isDev: Boolean, day: number, substitutionplanDay: SubstitutionPlan) => {
@@ -12,15 +14,27 @@ export const sendNotifications = async (isDev: Boolean, day: number, substitutio
 
         let users = getUsers().filter((user: User) => (!isDev || isDeveloper(user.username)) && user.grade !== undefined);
         console.log('Sending notifications to ' + users.length + ' users');
-        const notifications = new Map<string, Device[]>(); 
+        const notifications = new Map<string, Device[]>();
         for (let user of users) {
             try {
+                const substitutions = getSubstitutionsForUser(user, substitutionplanDay);
                 for (let device of user.devices) {
                     try {
                         if (!device.notifications) return;
-                        const text = device.language === 'de' ? 
-                            'Vertretungsplan wurde aktualisiert!' :
-                            'There is a new substitution plan!';
+                        var text = substitutions.map((s) => {
+                            const unsure = s.courseID === undefined && s.id === undefined;
+                            let text = ''
+                            if (unsure) text += '(';
+                            text += `${s.unit + 1}. Stunde ${getSubject(s.original.subjectID)} ${s.original.teacherID.toLocaleUpperCase()}`.trim();
+                            text += ': ';
+                            if (s.type === 0) text += 'Änderung';
+                            else if (s.type === 1) text += 'Freistunde';
+                            else if (s.type === 2) text += 'Klausur';
+                            if (unsure) text += ')';
+                            
+                            return text;
+                        }).join('\n');
+                        if (text.length === 0) text = 'Es gibt keine Änderungen für dich';
                         const title = getWeekday(weekday, device.language);
                         const notification = `${title}||${text}`;
                         if (!notifications.get(notification)) {
@@ -31,7 +45,7 @@ export const sendNotifications = async (isDev: Boolean, day: number, substitutio
                             devices.push(device);
                         }
                     }
-                    catch (e) { 
+                    catch (e) {
                         console.error(`Cannot send notification to ${user.username}'s device: ${device.firebaseId}:`, e);
                     }
                 }
@@ -39,7 +53,7 @@ export const sendNotifications = async (isDev: Boolean, day: number, substitutio
                 console.error('Cannot send notification to user: ', user.username, e);
             }
         }
-        
+
         // Send all notifications
         console.log(`Send ${notifications.size} different notifications`)
         for (var notification of Array.from(notifications.keys())) {
@@ -58,14 +72,14 @@ export const sendNotifications = async (isDev: Boolean, day: number, substitutio
                 console.error(`Cannot send notification:`, e);
             }
         }
-        
+
         await updateApp('All', {
             'type': 'substitution plan',
             'action': 'update',
             'day': day.toString(),
             'weekday': weekday.toString()
         }, isDev);
-        
+
     } catch (e) {
         console.error('Failed to send notifications', e);
     }
