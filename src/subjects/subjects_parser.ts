70f6d1d5bcbd9fcc337cfe0b7785f7d05cfe7c24
Finn <tupfi2@gmx.de>
2019-12-22 20:49:26 +0100

    Added manual subjects

--- src/subjects/subjects_parser.ts
+++ src/subjects/subjects_parser.ts
@@ -1,10 +1,15 @@
 import { Subjects } from '../utils/interfaces';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 export const extractData = (data: string[]): Subjects => {
     const subjects: Subjects = {};
-    //TODO: Override and add custom subject names
     data.filter((line) => line.startsWith('F1')).forEach((line) => {
         subjects[line.split(';')[2].toLowerCase()] = line.split(';')[3];
+    });
+    const fixSubjects = JSON.parse(readFileSync(resolve(process.cwd(), 'subjects.json')).toString());
+    Object.keys(fixSubjects).forEach((subject: string) => {
+        subjects[subject] = fixSubjects[subject];
     });
     return subjects;
 };
