d12fd64626f69911303928ceddd632ca9f9a2354
Finn <tupfi2@gmx.de>
2020-02-13 18:11:49 +0100

    Fixed multi device notifications

--- src/substitution_plan/sp_notifications.ts
+++ src/substitution_plan/sp_notifications.ts
@@ -22,70 +22,79 @@ export const sendNotifications = async (isDev: boolean, day: number, substitutio
         if (date.getTime() < current.getTime() && !(date.getDate() === current.getDate()
             && date.getMonth() === current.getMonth()
             && date.getFullYear() === current.getFullYear())) {
-            console.log('The day has passed, do not send notifications');
+            console.log(day + ': ' + 'The day has passed, do not send notifications');
             return;
         }
         const weekday = new Date(substitutionplanDay.date).getDay() - 1;
 
         let users = await getUsers(isDev);
-        console.log('Sending notifications to ' + users.length + ' users');
+        console.log(day + ': ' + 'Sending notifications to ' + users.length + ' users');
+        let notChanged = 0;
+        let deviceCount = 0;
         const notifications = new Map<string, Device[]>();
         for (let user of users) {
             try {
                 const substitutions = await getSubstitutionsForUser(user, substitutionplanDay);
+                
+                var text = substitutions.map((s) => {
+                    const unsure = s.courseID === undefined && s.id === undefined;
+                    let text = ''
+                    if (unsure) text += '(';
+                    text += `${s.unit + 1}. ${getLocalization('hour')} ${getSubject(s.original.subjectID)} ${s.original.teacherID.toLocaleUpperCase()}`.trim();
+                    text += ': ';
+                    if (s.type === 0) text += getLocalization('change');
+                    else if (s.type === 1) text += getLocalization('freeLesson');
+                    else if (s.type === 2) text += getLocalization('exam');
+                    if (unsure) text += ')';
+
+                    return text;
+                }).join('\n');
+                if (text.length === 0) text = getLocalization('noChanges');
+
+                /// Check if notification changed to last time
+                const newNotification = crypto.createHash('md5').update(text).digest('hex');
+                const notificationKey = `${new Date().getDate()}-${Math.floor(date.getTime() / 86400000)}-${newNotification}`;
+                const lastNotification = await getNotification(user.username, day);
+                if (!lastNotification || lastNotification !== notificationKey) {
+                    setNotification(user.username, day, notificationKey);
+                } else {
+                    notChanged++;
+                    if (!isDev) continue;
+                }
+
+                const title = getWeekday(weekday);
+                const notification = `${title}||${text}`;
+                if (!notifications.get(notification)) {
+                    notifications.set(notification, []);
+                }
+
                 const devices = await getDevices(user.username);
                 for (let device of devices) {
                     try {
+                        // Check if the device has notifications enabled
                         var getNotifications = await getPreference(device.firebaseId, 'spNotifications');
                         if (getNotifications === undefined) getNotifications = true;
                         if (!getNotifications) continue;
-                        var text = substitutions.map((s) => {
-                            const unsure = s.courseID === undefined && s.id === undefined;
-                            let text = ''
-                            if (unsure) text += '(';
-                            text += `${s.unit + 1}. ${getLocalization('hour')} ${getSubject(s.original.subjectID)} ${s.original.teacherID.toLocaleUpperCase()}`.trim();
-                            text += ': ';
-                            if (s.type === 0) text += getLocalization('change');
-                            else if (s.type === 1) text += getLocalization('freeLesson');
-                            else if (s.type === 2) text += getLocalization('exam');
-                            if (unsure) text += ')';
 
-                            return text;
-                        }).join('\n');
-                        if (text.length === 0) text = getLocalization('noChanges');
-
-                        /// Check if notification changed to last time
-                        const newNotification = crypto.createHash('md5').update(text).digest('hex');
-                        const notificationKey = `${new Date().getDate()}-${Math.floor(date.getTime() / 86400000)}-${newNotification}`;
-                        const lastNotification = await getNotification(user.username, day);
-                        if (!lastNotification || lastNotification !== notificationKey) {
-                            setNotification(user.username, day, notificationKey);
-                        } else {
-                            console.log(`Notification not changed for user ${user.username}`);
-                            if (!isDev) continue;
-                        }
-
-                        const title = getWeekday(weekday);
-                        const notification = `${title}||${text}`;
-                        if (!notifications.get(notification)) {
-                            notifications.set(notification, []);
-                        }
+                        // Set the notification for each device
                         const devices = notifications.get(notification);
                         if (devices) {
+                            deviceCount++;
                             devices.push(device);
                         }
                     }
                     catch (e) {
-                        console.error(`Cannot send notification to ${user.username}'s device: ${device.firebaseId}:`, e);
+                        console.error(day + ': ' + `Cannot send notification to ${user.username}'s device: ${device.firebaseId}:`, e);
                     }
                 }
             } catch (e) {
-                console.error('Cannot send notification to user: ', user.username, e);
+                console.error(day + ': ' + 'Cannot send notification to user: ', user.username, e);
             }
         }
 
         // Send all notifications
-        console.log(`Send ${notifications.size} different notifications`)
+        console.log(day + ': ' + `Ignore ${notChanged} notifications, because they did not changed`);
+        console.log(day + ': ' + `Send ${notifications.size} different notifications to ${deviceCount} different devices`);
         for (var notification of Array.from(notifications.keys())) {
             try {
                 const changesCount = notification.split('|')[1].split('\n').length;
